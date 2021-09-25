60aa24a415dfcf67395563015eb6cd10c8b4a232
Tigr046 <tigranadtbekyan@yandex.ru>
2021-09-25 21:17:36 +0300

    Add custom authorization

---
+++ Articles/CustomAttribute/CustomAuthorization.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Articles.CustomAttribute
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class CustomAuthorization : Attribute, IAuthorizationFilter
+    {
+        List<string> allowedRoles = new List<string>();
+        public CustomAuthorization(string Roles)
+        {
+            allowedRoles = Roles.Split(',').ToList();
+        }
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            foreach(string allowedRole in allowedRoles)
+            {
+                if (context.HttpContext.User.IsInRole(allowedRole))
+                {
+                    return;
+                }
+            }
+            context.Result = new ForbidResult(CookieAuthenticationDefaults.AuthenticationScheme);
+            
+        }
+    }
+}
